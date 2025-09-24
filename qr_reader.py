f0915e9201583290a1fceb087f6e42b56fc15a67
Inpayo <1np4y0@gmail.com>
2025-09-24 13:42:50 +0800

    Camera scan until QR is detected

--- qr_reader.py
+++ qr_reader.py
@@ -1,28 +1,22 @@
 from qreader import QReader
-from time import process_time
 
 
 qreader = QReader()
-reading = False
+
 
 
 def qr(img):
-    global reading
     data = qreader.detect_and_decode(image=img)
     if data:
-        if reading:
-            end_count = process_time()
-            time_diff = end_count - start_count
-            if time_diff >= 5:
-                data = qreader.detect_and_decode(image=img)
-                print("QR detected: ")
-                for i in data:
-                    print (i)
-                
-                return True
-        else:
-            start_count = process_time()
-            reading = True
+        data = qreader.detect_and_decode(image=img)
+        for i in data:
+            if i == None:
+                print("Please bring the QR closer to the camera")
+                break
+            print (i)
+            return True
+
+
 
  
     return False
