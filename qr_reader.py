693f1c2c7341a0b8f9b8273b91a0c6ab0cf7ade3
Inpayo <1np4y0@gmail.com>
2025-09-26 20:09:48 +0800

    chore: cleaning

--- qr_reader.py
+++ qr_reader.py
@@ -9,14 +9,13 @@ def qr(img):
     data = qreader.detect_and_decode(image=img) #Scans the QR code
     if data: #if QR code was detected
         for i in data:
+
             if i == None: #Occurs when scanned code is invalid
                 print("Please bring the QR closer to the camera")
                 break #Code continues running
+
             print (i) #Outputs the decoded QR code
-            open(i)
+            open(i) #opens link in web browser, will replace this later
+
             return True
-
-
-
- 
     return False
